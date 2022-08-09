c59f4c635d5545750c15b8335e65bf63337c3e20
Ryan Garver <ryan.garver@gmail.com>
2022-08-09 05:25:29 -0400

    v1.0

--- genetic_n_queens.rb
+++ genetic_n_queens.rb
@@ -2,8 +2,8 @@
 # Usage: `ruby genetic_n_queens.rb`
 # See Config.help() for command line options
 
-require_relative 'genetic_n_queens/config'
-require_relative 'genetic_n_queens/solution'
+require_relative 'lib/config'
+require_relative 'lib/solution'
 
 Config.parse_options(ARGV.each_slice(2).to_h) rescue Config.help()
 Solution.solve()
